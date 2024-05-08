1c08cf63b616fd77f32356f8a93463245c43f3c1
Nurlana <nurlana@cubics.technology>
2024-05-08 17:05:44 +0400

    upload image

--- shared/hooks/UseModalOpen.js
+++ shared/hooks/UseModalOpen.js
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-export  function openCloseModal(){
+export  function useModalOpen(){
     const [isOpen,setIsOpen]=useState()
     function onOpen(){
         setIsOpen(true)
