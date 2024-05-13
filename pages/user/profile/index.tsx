d43f5eb67b1014aa2ff2e87161c413a220bfbd64
mahooo <sevinmuhammed06@gmail.com>
2024-05-13 11:20:41 +0400

    add navbar for user profile

---
+++ pages/user/profile/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../../../shared/components/client/Header/Header';
+import Nav from '../../../shared/components/client/Nav/Nav';
+import Footer from '../../../shared/components/client/Footer/Footer';
+import Navbar from '../../../shared/components/Client/user-NAV';
+
+function Profile() {
+    return (
+        <div>
+            <div className=' p-8'>
+            <Header/>
+            <Navbar active={1}/>
+            </div>
+            
+            <footer className=' bg-black w-full  h-96'>
+                footer
+            </footer>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ shared/components/Client/user-NAV/index.tsx
@@ -0,0 +1,86 @@
+import Image from 'next/image';
+import React from 'react';
+import Activeprofileicon from '../../../../public/Activeprofile.svg'
+import ActiBasketIcon from '../../../../public/activeBasket.svg'
+import defBasketIcon from'../../../../public/defaultBasket.svg'
+import defPRofileIcon from'../../../../public/defProfile.svg'
+import styles from '../user-NAV/Usernav.module.css'
+import { useRouter } from 'next/router';
+interface PROPs{
+    active:number
+}
+function Navbar(props:PROPs) {
+    const ruter =useRouter()
+    let {active} =props
+    return (
+        <div className={styles.navbar}>
+            <div
+             className={ active===1? styles.icondiv+ ' '+styles.activediv: styles.icondiv }
+             onClick={()=>ruter.push('/user/profile')}
+             >
+                <Image
+                src={ active===1? Activeprofileicon:defPRofileIcon}
+                alt='Activeprofileicon'
+                width={24}
+                height={24}
+                />
+                <h3 className={ active===1? styles.icondiv+ ' '+styles.activeText: styles.defaultText }>Profile</h3>
+
+            </div>
+            <div 
+            className={ active===2? styles.icondiv+ ' '+styles.activediv: styles.icondiv }
+            onClick={()=>ruter.push('/user/basked')}
+            >
+                <Image
+                src={active===2?ActiBasketIcon:defBasketIcon}
+                alt='defBasketIcon'
+                width={24}
+                height={24}
+                />
+                <h3 className={active===2? styles.icondiv+ ' '+styles.activeText: styles.defaultText }>Your Basket</h3>
+
+            </div>
+            <div 
+            className={active===3? styles.icondiv+ ' '+styles.activediv: styles.icondiv }
+            onClick={()=>ruter.push('/user/orders')}
+            >
+                <Image
+                src={active===3?ActiBasketIcon:defBasketIcon}
+                alt='defBasketIcon'
+                width={24}
+                height={24}
+                />
+                <h3 className={active===3? styles.icondiv+ ' '+styles.activeText: styles.defaultText }>Your Orders </h3>
+
+            </div>
+            <div
+             className={active===4? styles.icondiv+ ' '+styles.activediv: styles.icondiv }
+             onClick={()=>ruter.push('/user/checkoyth')}
+             >
+                <Image
+                src={active===4?ActiBasketIcon:defBasketIcon}
+                alt='defBasketIcon'
+                width={24}
+                height={24}
+                />
+                <h3 className={active===4? styles.icondiv+ ' '+styles.activeText: styles.defaultText }>Checkout</h3>
+
+            </div>
+            <div 
+            className={active===5? styles.icondiv+ ' '+styles.activediv: styles.icondiv }
+            onClick={()=>ruter.push('/user/logouth')}
+            >
+                <Image
+                src={active===5?ActiBasketIcon:defBasketIcon}
+                alt='defBasketIcon'
+                width={24}
+                height={24}
+                />
+                <h3 className={active===5? styles.icondiv+ ' '+styles.activeText: styles.defaultText }>Logout</h3>
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
