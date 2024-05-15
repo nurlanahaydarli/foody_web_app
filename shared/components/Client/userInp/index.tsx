41561ced5f4d5a5c16a52e0d6152369d0d60f7c4
Nurlana <nurlana@cubics.technology>
2024-05-15 18:27:45 +0400

    last version

--- shared/components/Client/userInp/index.tsx
+++ shared/components/Client/userInp/index.tsx
@@ -1,21 +1,23 @@
-import { ErrorMessage, Field } from 'formik';
+import {ErrorMessage, Field} from 'formik';
 import React from 'react';
 import style from '../userInp/input.module.css'
-interface Props{
-    name:string,
-    type:string,
-    pleaceholder:string,
-    title:string,
+
+interface Props {
+    name?: string,
+    type?: string,
+    placeholder?: string,
+    title?: string,
 }
-function Input(props:Props) {
-let {label,inp,eror}=style
-let {name,type,pleaceholder,title} =props
+
+function Input(props: Props) {
+    let {label, inp, eror} = style
+    let {name, type, placeholder, title} = props
     return (
         <div className='flex flex-col'>
             <label htmlFor={name} className={label}>{title}</label>
-            <Field type={type} id={name} name={name} className={inp} placeholder={pleaceholder}/>
-            <ErrorMessage name={name} component="div" className={eror} />
-          </div>
+            <Field type={type} id={name} name={name} className={inp} placeholder={placeholder}/>
+            <ErrorMessage name={name} component="div" className={eror}/>
+        </div>
     );
 }
 
