191d4b421465919fd8e4cc98f30c89dddda2fac4
Nurlana <nurlana@cubics.technology>
2024-05-15 17:42:32 +0400

    fix:deployment

--- shared/components/client/auth/Auth.jsx
+++ shared/components/client/auth/Auth.jsx
@@ -1,11 +1,13 @@
 import {useRouter} from "next/router";
 import ButtonWeb from "../Button/ButtonWeb";
+import styles from '../Button/btn.module.css'
 
 export default function Auth(){
     let {push}=useRouter()
     return(
         <>
-             <ButtonWeb typeButton={true} title={'Sign up'} btnSize={'sm'} addButton={false} />
+             {/*<ButtonWeb typeButton={true} title={'Sign up'} btnSize={'sm'} addButton={false} />*/}
+            <button className={`${styles.btn} ${styles.btn_sm} ${styles.main}`} >Sign up</button>
         </>
     )
 }
