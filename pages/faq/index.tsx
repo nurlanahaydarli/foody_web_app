d08461a1e55a0fc73a70d3f3b357ff1d9cf3a21e
Nurlana <nurlana@cubics.technology>
2024-05-22 11:13:53 +0400

    test deployment part 13

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -4,7 +4,10 @@ import React, { useState } from "react";
 
 import Image from "next/image";
 import MainLayout from "../../shared/components/admin/Layout/MainLayout";
-
+import Modal1 from "../../shared/components/Client/FaqsModal1/index";
+import Modal2 from "../../shared/components/Client/FaqsModal2/index";
+import Modal3 from "../../shared/components/Client/FaqsModal3/index";
+import Modal4 from "../../shared/components/Client/FaqsModal4/index";
 
 function Faq() {
   const [isContentVisible, setContentVisibility] = useState(false);
@@ -16,7 +19,10 @@ function Faq() {
     <MainLayout>
       <main className="my-[60px] flex flex-col items-center gap-5 ">
         <h1 className=" text-5xl font-medium pb-5">F.A.Q</h1>
-          test
+        <Modal1 />
+        <Modal2 />
+        <Modal3 />
+        <Modal4 />
       </main>
     </MainLayout>
   );
