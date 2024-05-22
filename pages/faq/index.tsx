c3e71c95574841729e59aa0572edf685572508f8
Nurlana <nurlana@cubics.technology>
2024-05-22 08:11:23 +0400

    test deploy 2

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 import Image from "next/image";
 import MainLayout from "../../shared/components/admin/Layout/MainLayout";
-import Modal1 from "../../shared/components/Client/faqsModal1";
+// import Modal1 from "../../shared/components/Client/faqsModal1";
 // import Modal2 from "../../shared/components/Client/faqsModal2";
 // import Modal3 from "../../shared/components/Client/faqsModal3";
 // import Modal4 from "../../shared/components/Client/faqsModal4";
@@ -19,7 +19,7 @@ function Faq() {
     <MainLayout>
       <main className="my-[60px] flex flex-col items-center gap-5 ">
         <h1 className=" text-5xl font-medium pb-5">F.A.Q</h1>
-        <Modal1 />
+        {/*<Modal1 />*/}
         {/*<Modal2 />*/}
         {/*<Modal3 />*/}
         {/*<Modal4 />*/}
