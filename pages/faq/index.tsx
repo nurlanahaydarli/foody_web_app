78bfd795c727127404bdc32df4a06c29c5395bfe
Nurlana <nurlana@cubics.technology>
2024-05-22 11:02:49 +0400

    test deployment  part 12

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 
 import Image from "next/image";
 import MainLayout from "../../shared/components/admin/Layout/MainLayout";
-// import Modal1 from "../../shared/components/Client/FaqsModal1";
-// import Modal2 from "../../shared/components/Client/FaqsModal2";
-// import Modal3 from "../../shared/components/Client/FaqsModal3";
-// import Modal4 from "../../shared/components/Client/FaqsModal4";
+import Modal1 from "../../shared/components/Client/FaqsModal1";
+import Modal2 from "../../shared/components/Client/FaqsModal2";
+import Modal3 from "../../shared/components/Client/FaqsModal3";
+import Modal4 from "../../shared/components/Client/FaqsModal4";
 
 function Faq() {
   const [isContentVisible, setContentVisibility] = useState(false);
@@ -19,10 +19,10 @@ function Faq() {
     <MainLayout>
       <main className="my-[60px] flex flex-col items-center gap-5 ">
         <h1 className=" text-5xl font-medium pb-5">F.A.Q</h1>
-        {/*<Modal1 />*/}
-        {/*<Modal2 />*/}
-        {/*<Modal3 />*/}
-        {/*<Modal4 />*/}
+        <Modal1 />
+        <Modal2 />
+        <Modal3 />
+        <Modal4 />
       </main>
     </MainLayout>
   );
