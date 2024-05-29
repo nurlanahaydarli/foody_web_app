c9303ff9dc56ffe753779db74a0ac0c88d71992a
Nurlana <nurlana@cubics.technology>
2024-05-29 11:49:12 +0400

    add new chart to dashboard

---
+++ shared/components/admin/ChartCard/LineChart.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import ReactApexChart from "react-apexcharts";
+import styles from "./card.module.css";
+
+export default function LineChart() {
+
+    const [series] = useState([
+        {
+            name: "Sample Data",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }])
+
+    const [options] = useState<ApexCharts.ApexOptions>({
+        chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'],
+                opacity: 0.5
+            },
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        }
+    });
+    return (
+        <>
+            <div className={styles.card_item}>
+                <div className={styles.card_box}>
+                    <h3 className={styles.card_title}>Restaurants</h3>
+                    <ReactApexChart
+                        options={options}
+                        series={series}
+                        type="line"
+                        height={350}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
