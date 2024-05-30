9dd2ef1a3ce8c807be6865794c8151d1f2f6be71
Nurlana <nurlana@cubics.technology>
2024-05-30 08:31:46 +0400

    find product bug

--- shared/components/Client/Products/ProductCard.tsx
+++ shared/components/Client/Products/ProductCard.tsx
@@ -5,19 +5,18 @@ import React, {useEffect, useState} from "react";
 import {useMutation, useQuery, useQueryClient} from "react-query";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../redux/store";
-import {AddBasket, GetBasket} from "../../../services";
+import {AddBasket} from "../../../services";
 import {BasketPostDataType} from "../../../interfaces";
 import { ToastContainer, toast } from 'react-toastify';
 
 type ProductState = {
-    id: string | undefined;
-    description?: string | undefined;
-    img_url: string | undefined;
-    name: string | undefined;
-    price: number | undefined;
+    id: string ;
+    description?: string ;
+    img_url: string ;
+    name: string ;
+    price: number ;
 }
 export default function ProductsCard(product: ProductState) {
-    console.log(product,'product')
     let {id, description, img_url, name, price} = product;
     const queryClient = useQueryClient();
     const [buttonClicked, setButtonClicked] = useState(false);
@@ -56,41 +55,43 @@ export default function ProductsCard(product: ProductState) {
             user_id: user.id,
             product_id: product.id,
         };
-        if(user){
+        if (user) {
             setButtonClicked(true);
             mutation.mutate(basketProduct);
-            toast.success("successfully!", {
-                autoClose: 1000,
-            });
+            // toast.success("Successfully!", {
+            //     autoClose: 1000,
+            // });
         }
-        if(!user){
+        if (!user) {
             setButtonClicked(false);
-            toast.error("Error ", {
-                autoClose: 1000,
-            });
+            // toast.error("Error ", {
+            //     autoClose: 1000,
+            // });
 
-    };
-    return (
-        <>
-            <div className={`flex justify-between sm:flex-nowrap flex-wrap  gap-[12px] ${styles.product_box}`}>
-                <div
-                    className={`flex justify-start md:flex-nowrap flex-wrap md:gap-[30px] gap-2 ${styles.product_left}`}>
-                    <img src={img_url} alt={name} className='w-[60px] h-[60px]'/>
-                    <div className={styles.left_text}>
-                        <h4>
-                            {name}
-                        </h4>
-                        {description && <p>{description}</p>}
+        }}
+
+        return (
+            <>
+                <div className={`flex justify-between sm:flex-nowrap flex-wrap  gap-[12px] ${styles.product_box}`}>
+                    <div
+                        className={`flex justify-start md:flex-nowrap flex-wrap md:gap-[30px] gap-2 ${styles.product_left}`}>
+                        <img src={img_url} alt={name} className='w-[60px] h-[60px]'/>
+                        <div className={styles.left_text}>
+                            <h4>
+                                {name}
+                            </h4>
+                            {description && <p>{description}</p>}
+                        </div>
+                    </div>
+                    <div className={`flex justify-end items-center md:gap-[30px] gap-2 ${styles.product_right}`}>
+                        <p>
+                            From <span>{price} &#8380;</span>
+                        </p>
+                        <button onClick={handleAddToBasket} className={buttonClicked ? styles.active : ''}><PlusSvg/>
+                        </button>
                     </div>
                 </div>
-                <div className={`flex justify-end items-center md:gap-[30px] gap-2 ${styles.product_right}`}>
-                    <p>
-                        From <span>{price} &#8380;</span>
-                    </p>
-                    <button onClick={handleAddToBasket} className={buttonClicked ? styles.active : ''}><PlusSvg/></button>
-                </div>
-            </div>
 
-        </>
-    )
-    }}
+            </>
+        )
+    }
