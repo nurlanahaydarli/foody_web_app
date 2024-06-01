73becc992245cf5be1ee0213370b0196884a7933
Nurlana <nurlana@cubics.technology>
2024-06-01 18:57:28 +0400

    sort data

--- shared/utils/sortData.ts
+++ shared/utils/sortData.ts
@@ -1,3 +1,3 @@
-export function sortDataByCreated(data: any[]) {
+export function sortDataByCreated(data?: any[] | undefined) {
     return data?.sort((a: any, b: any) => new Date(b.created).getTime() - new Date(a.created).getTime());
 }
