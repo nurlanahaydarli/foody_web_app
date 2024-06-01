031a39e2934cf98f7c186c74080bac0f366f77e2
Nurlana <nurlana@cubics.technology>
2024-06-01 13:55:10 +0400

    use fetch data custom hook part 1 and add spinner

--- shared/hooks/UseFetchData.tsx
+++ shared/hooks/UseFetchData.tsx
@@ -1,31 +1,62 @@
-import { useState } from "react";
-import { toast } from "react-toastify";
+import {useState} from "react";
+import {toast} from "react-toastify";
+
+interface UseEntityHandlerProps {
+    uploadFile:  (params: { file: File; collectionId: string; documentId: string }) => Promise<string | null>;
+    addEntity: (entity: object) => Promise<any>;
+    editEntity: (entity: object, id: string | number) => Promise<any>;
+    deleteEntity: (id: string | number) => Promise<any>;
+    onClose: () => void;
+}
 
 export const useEntityHandler = ({
-                                     uploadFile,
+                                     uploadFile ,
                                      addEntity,
                                      editEntity,
                                      deleteEntity,
                                      onClose
-                                 }) => {
+                                 }: UseEntityHandlerProps) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const handleEntity = async ({ entity, Img, setEntities, inputs, isEdit, editID }) => {
+    const handleEntity = async ({
+                                    entity,
+                                    Img,
+                                    editImg,
+                                    setEntities,
+                                    inputs,
+                                    isEdit,
+                                    editID,
+                                    collectionId,
+                                    documentId
+                                }: {
+        entity: { [key: string]: any };
+        Img: File | null;
+        editImg?: File | null;
+        setEntities: React.Dispatch<React.SetStateAction<any[]>>;
+        inputs: { [key: string]: React.RefObject<HTMLInputElement> };
+        isEdit: boolean;
+        editID?: string | number | null;
+        collectionId: string;
+        documentId: string;
+    }) => {
         setLoading(true);
         setError(null);
         try {
             if (Img instanceof File) {
                 const res = await uploadFile({
                     file: Img,
-                    collectionId: entity.collectionId, // Adjusted to include collectionId dynamically
-                    documentId: entity.documentId // Adjusted to include documentId dynamically
+                    collectionId: collectionId,
+                    documentId: documentId
                 });
                 entity.img_url = res;
+            } else if (!Img && isEdit) {
+                entity.img_url = editImg;
             }
 
             if (isEdit) {
                 await editEntity(entity, editID);
+                setEntities(prev => prev.map(item => (item.id === editID ? { ...item, ...entity } : item)));
                 toast.success("Entity successfully edited", {
                     position: "top-right",
                 });
@@ -46,22 +77,28 @@ export const useEntityHandler = ({
                 position: "top-right",
             });
             setError(err);
-            console.log(err);
         } finally {
             setLoading(false);
         }
     };
 
-    const removeEntity = async (id) => {
+    const removeEntity = async (id: string | number, setEntities: React.Dispatch<React.SetStateAction<any[]>>) => {
+        setLoading(true);
         try {
+            console.log(id,'id')
             await deleteEntity(id);
-            toast.success("Entity successfully deleted", {
-                position: "top-right",
-            });
+            setEntities(prev => prev.filter((entity)=>{
+                return  entity.id !== id
+            }))
+            toast.success("Entity successfully deleted", { position: "top-right" });
         } catch (err) {
-            console.log(err);
+            setError(err);
+            // toast.error("An error occurred while deleting the entity", { position: "top-right" });
+        } finally {
+            setLoading(false);
         }
     };
 
-    return { handleEntity, removeEntity, loading, error };
+
+    return {handleEntity, removeEntity, loading, error};
 };
