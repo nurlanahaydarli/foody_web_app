986585b10bef92ed741b62a72976af7c0febf6e3
Nurlana <nurlana@cubics.technology>
2024-06-02 09:57:56 +0400

    fix restaurants request

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,20 @@
 // _app.js
-
-import firebase from 'firebase/app';
-import 'firebase/firestore'; // Импортируйте необходимые службы Firebase
-
-// Инициализация Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBhEEy4M3b5XRKF6fae1yMnvgf8PyFBljs",
-    authDomain: "foody-web-app-9e4ea.firebaseapp.com",
-    databaseURL: "https://foody-web-app-9e4ea-default-rtdb.firebaseio.com",
-    projectId: "foody-web-app-9e4ea",
-    storageBucket: "foody-web-app-9e4ea.appspot.com",
-    messagingSenderId: "388680784127",
-    appId: "1:388680784127:web:4fe1126550707d497254ca",
-    measurementId: "G-G7FYWM6DQ1"
-  };
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+//
+// import firebase from 'firebase/app';
+// import 'firebase/firestore'; // Импортируйте необходимые службы Firebase
+//
+// // Инициализация Firebase
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBhEEy4M3b5XRKF6fae1yMnvgf8PyFBljs",
+//     authDomain: "foody-web-app-9e4ea.firebaseapp.com",
+//     databaseURL: "https://foody-web-app-9e4ea-default-rtdb.firebaseio.com",
+//     projectId: "foody-web-app-9e4ea",
+//     storageBucket: "foody-web-app-9e4ea.appspot.com",
+//     messagingSenderId: "388680784127",
+//     appId: "1:388680784127:web:4fe1126550707d497254ca",
+//     measurementId: "G-G7FYWM6DQ1"
+//   };
+//
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
--- server/configs/firebase.js
+++ server/configs/firebase.js
@@ -6,23 +6,23 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC1084Ak7OamlsTvjkAlyVYuh3NNvfuqzc",
-//     authDomain: "foody-3.firebaseapp.com",
-//     projectId: "foody-3",
-//     storageBucket: "foody-3.appspot.com",
-//     messagingSenderId: "962449069028",
-//     appId: "1:962449069028:web:63e285cc26c17e4a0aedf1"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC68IobQTT7kIpF39ZOAdZFcgv75doxD50",
-    authDomain: "foody-4.firebaseapp.com",
-    projectId: "foody-4",
-    storageBucket: "foody-4.appspot.com",
-    messagingSenderId: "745316669318",
-    appId: "1:745316669318:web:84ab8b0b32d51120717e07"
+    apiKey: "AIzaSyC1084Ak7OamlsTvjkAlyVYuh3NNvfuqzc",
+    authDomain: "foody-3.firebaseapp.com",
+    projectId: "foody-3",
+    storageBucket: "foody-3.appspot.com",
+    messagingSenderId: "962449069028",
+    appId: "1:962449069028:web:63e285cc26c17e4a0aedf1"
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC68IobQTT7kIpF39ZOAdZFcgv75doxD50",
+//     authDomain: "foody-4.firebaseapp.com",
+//     projectId: "foody-4",
+//     storageBucket: "foody-4.appspot.com",
+//     messagingSenderId: "745316669318",
+//     appId: "1:745316669318:web:84ab8b0b32d51120717e07"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
