28a1a537fb734834f6555cad7820635642cf1563
Nurlana <nurlana@cubics.technology>
2024-06-02 16:38:47 +0400

    refix products and restaurants requests

--- server/configs/firebaseAdmin.js
+++ server/configs/firebaseAdmin.js
@@ -2,8 +2,8 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-    const serviceAccount = require("./foody-3-firebase-adminsdk-8atpy-6897ff2d7c.json");
-    // const serviceAccount = require("./foody-4-firebase-adminsdk-wq6z7-7cd55c1f87.json");
+    // const serviceAccount = require("./foody-3-firebase-adminsdk-8atpy-6897ff2d7c.json");
+    const serviceAccount = require("./foody-4-firebase-adminsdk-wq6z7-7cd55c1f87.json");
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
--- shared/components/Client/Products/ProductCard.tsx
+++ shared/components/Client/Products/ProductCard.tsx
@@ -1,36 +1,36 @@
-import styles from './products.module.css'
-import Image from "next/image";
-import PlusSvg from "../svg/PlusSvg";
-import React, {useEffect, useState} from "react";
-import {useMutation, useQuery, useQueryClient} from "react-query";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../redux/store";
-import {AddBasket} from "../../../services";
-import {BasketPostDataType} from "../../../interfaces";
+import styles from './products.module.css';
+import React, { useEffect, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+import { AddBasket, GetBasket } from '../../../services';
+import { BasketPostDataType } from '../../../interfaces';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PlusSvg from "../svg/PlusSvg";
 
 type ProductState = {
-    id: string ;
-    description?: string ;
-    img_url: string ;
-    name: string ;
-    price: number ;
-}
+    id: string;
+    description?: string;
+    img_url: string;
+    name: string;
+    price: number;
+};
+
 export default function ProductsCard(product: ProductState) {
-    let {id, description, img_url, name, price} = product;
+    let { id, description, img_url, name, price } = product;
     const queryClient = useQueryClient();
     const [buttonClicked, setButtonClicked] = useState(false);
     const user = useSelector((state: RootState) => state.user);
-    // const {data: basket_data} = useQuery('basket',GetBasket)
-    // useEffect(()=>{
-    //     basket_data?.data.result.data.items.filter((basket_product)=>{
-    //         if(basket_product.id === id){
-    //             return setButtonClicked(true)
-    //         }else{
-    //             return setButtonClicked(false)
-    //         }
-    //     })
-    // },[])
+
+    const { data: basketData } = useQuery('basket', GetBasket);
+
+    useEffect(() => {
+        if (basketData) {
+            const isInBasket = basketData.data.result.data.items.some((basketProduct: any) => basketProduct.product_id === id);
+            setButtonClicked(isInBasket);
+        }
+    }, [basketData, id]);
 
     const mutation = useMutation(
         (basketProduct: BasketPostDataType) => AddBasket(basketProduct),
@@ -43,55 +43,49 @@ export default function ProductsCard(product: ProductState) {
             },
             onError: (error) => {
                 console.error("Error adding product to the basket:", error);
-                setButtonClicked(false)
+                setButtonClicked(false);
                 toast.error("Error adding product to the basket", {
                     autoClose: 1000,
                 });
             },
         }
     );
+
     const handleAddToBasket = () => {
+        if (!user) {
+            toast.error("Please log in to add products to the basket", {
+                autoClose: 1000,
+            });
+            return;
+        }
+
         const basketProduct: BasketPostDataType = {
             user_id: user.id,
-            product_id: product.id,
+            product_id: id,
         };
-        if (user) {
-            setButtonClicked(true);
-            mutation.mutate(basketProduct);
-            // toast.success("Successfully!", {
-            //     autoClose: 1000,
-            // });
-        }
-        if (!user) {
-            setButtonClicked(false);
-            // toast.error("Error ", {
-            //     autoClose: 1000,
-            // });
 
-        }}
+        setButtonClicked(true);
+        mutation.mutate(basketProduct);
+    };
 
-        return (
-            <>
-                <div className={`flex justify-between sm:flex-nowrap flex-wrap  gap-[12px] ${styles.product_box}`}>
-                    <div
-                        className={`flex justify-start md:flex-nowrap flex-wrap md:gap-[30px] gap-2 ${styles.product_left}`}>
-                        <img src={img_url} alt={name} className='w-[60px] h-[60px]'/>
-                        <div className={styles.left_text}>
-                            <h4>
-                                {name}
-                            </h4>
-                            {description && <p>{description}</p>}
-                        </div>
-                    </div>
-                    <div className={`flex justify-end items-center md:gap-[30px] gap-2 ${styles.product_right}`}>
-                        <p>
-                            From <span>{price} &#8380;</span>
-                        </p>
-                        <button onClick={handleAddToBasket} className={buttonClicked ? styles.active : ''}><PlusSvg/>
-                        </button>
+    return (
+        <>
+            <div className={`flex justify-between sm:flex-nowrap flex-wrap gap-[12px] ${styles.product_box}`}>
+                <div className={`flex justify-start md:flex-nowrap flex-wrap md:gap-[30px] gap-2 ${styles.product_left}`}>
+                    <img src={img_url} alt={name} className='w-[60px] h-[60px]' />
+                    <div className={styles.left_text}>
+                        <h4>{name}</h4>
+                        {description && <p>{description}</p>}
                     </div>
                 </div>
-
-            </>
-        )
-    }
+                <div className={`flex justify-end items-center md:gap-[30px] gap-2 ${styles.product_right}`}>
+                    <p>From <span>{price} &#8380;</span></p>
+                    <button onClick={handleAddToBasket} className={buttonClicked ? styles.active : ''}>
+                        <PlusSvg />
+                    </button>
+                </div>
+            </div>
+            <ToastContainer />
+        </>
+    );
+}
