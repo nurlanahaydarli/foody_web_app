78281c37f146d230870c389d931440895360ef5e
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-06-05 18:13:16 +0400

    test

--- shared/components/admin/AdminRestorant/index.tsx
+++ shared/components/admin/AdminRestorant/index.tsx
@@ -183,7 +183,7 @@ function AdminRestaurant() {
     let newRestaurant:any = {
       name: Title,
       img_url: '',
-      category_id: categorysID,
+      category_id: selectedCategoryName,
       cuisine: Cuisine,
       address: Adress,
       delivery_min: DeliveryMin,
@@ -479,7 +479,7 @@ function AdminRestaurant() {
         <Input onChange={()=>console.log('onChange')} hasLabel={true} title={'Name'} type={'text'} input_name={'restaurant_title'} Ref={inpTitle} value={TitleValue} />
         <div className=" text-mainRed font-bold">{TitleStroge}</div>
      
-        <Select value={selectedCategoryName} onChange={getCategoryById} title={"Categorys"} name={"cat_id"} options={categorys}  />
+        <Select  onChange={getCategoryById} title={"Categorys"} name={"cat_id"} options={categorys}  />
 
         <Input onChange={()=>console.log('onChange')} hasLabel={true} title={'Cuisine'} type={'text'} input_name={'restaurant_cuisine'} Ref={inpCuisine} value={CuisineValue} />
         <div className="text-mainRed font-bold">{CuisineStroge}</div>
